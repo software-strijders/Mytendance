f92036bfd2a126a645629d15223d96726f41186c
JortWillemsen <jort@maurice-nienke.nl>
2020-03-31 13:04:30 +0200

    Made changes to Attendance and Lecture tests -Changed naming of the tests -Added 2 more tests to LectureTest -Added spaces in method names -Made changes to the ID generation. No duplicate ID's are allowed.

--- test/models/AttendanceTest.java
+++ test/models/AttendanceTest.java
@@ -16,57 +16,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AttendanceTest {
 
-    private Student student = new Student("firstname", "lastname", "email",
-            "password");
-    Attendance a1 = new Attendance(ReasonType.ENTOMBMENT,
-            "Ik word begraven", student, AttendanceType.ABSENT);
+    private Student student;
+    Attendance a1;
+
+    @BeforeEach
+    void beforeEachTest() {
+        student = new Student("firstname", "lastname", "email", "password");
+        a1 = new Attendance(ReasonType.ENTOMBMENT, "Ik word begraven", student, AttendanceType.ABSENT);
+    }
 
     @Test
-    void testgetreasonType() {
+    void testReturnValueOfReasonType() {
         assertEquals(ReasonType.ENTOMBMENT, a1.getReason());
     }
 
     @Test
-    void testReasonTypeWhenReasonNull() {
+    void reasonTypeShouldReturnNullWhenNull() {
         Attendance a2 = new Attendance(null, "null", student, AttendanceType.ABSENT);
         assertNull(a2.getReason());
     }
 
     @Test
-    void testDescription(){
+    void testReturnValueOfDescription() {
         assertEquals("Ik word begraven", a1.getReasonDescription());
     }
 
     @Test
-    void testDescriptionWhenNull(){
+    void descriptionShouldReturnNullWhenNull() {
         Attendance a2 = new Attendance(ReasonType.ENTOMBMENT, null, student, AttendanceType.ABSENT);
         assertNull(a2.getReasonDescription());
     }
 
     @Test
-    void testStudent(){
+    void testReturnValueOfStudent() {
         assertEquals(student, a1.getStudent());
     }
 
     @Test
-    void testStudentWhenNull() {
+    void studentShouldReturnNullWhenNull() {
         Attendance a2 = new Attendance(ReasonType.ENTOMBMENT, "null", null, AttendanceType.ABSENT);
         assertNull(a2.getStudent());
     }
 
     @Test
-    void testAttendanceType() {
+    void testReturnValueOfAttendanceType() {
         assertEquals(AttendanceType.ABSENT, a1.getAttendanceType());
     }
 
     @Test
-    void testAttendanceTypeWhenNull() {
+    void attendanceTypeShouldReturnNullWhenNull() {
         Attendance a2 = new Attendance(ReasonType.ENTOMBMENT, "null", student, null);
         assertNull(a2.getAttendanceType());
     }
 
     @Test
-    void testToString(){
-        assertEquals(a1.toString(), "Firstname Lastname - ENTOMBMENT");
+    void attendanceToStringShouldReturnNullWhenNull() {
+        assertEquals("Firstname Lastname - ENTOMBMENT", a1.toString());
     }
 }
