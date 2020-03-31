1a50e8283634a484bf55a275c024553958d12dd9
Rubenvdbrink <rubenvandenbrink@hotmail.com>
2020-03-31 18:30:38 +0200

    Changed some things suggested by Xander.

--- src/controllers/AdministratorMenuController.java
+++ src/controllers/AdministratorMenuController.java
@@ -2,7 +2,6 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import models.user.User;
 import utils.FXUtils;
@@ -11,33 +10,20 @@ import java.io.IOException;
 
 public class AdministratorMenuController {
 
-    @FXML private Button createUserButton;
-    @FXML private Button createClassButton;
-    @FXML private Button createLectureButton;
-    @FXML private Button logOutButton;
     @FXML private Label loggedUserLabel;
 
     @FXML
-    void initialize() {
+    public void initialize() {
         loggedUserLabel.setText(String.valueOf(User.getLoggedInUser()));
     }
 
     @FXML
-    void onCreateClassClick(ActionEvent event) throws IOException {
-        FXUtils.loadComponent("Klas aanmaken","/views/CreateClassWindow.fxml", event);
+    private void onCreateUserClick(ActionEvent event) throws IOException {
+        FXUtils.loadComponent("Gebruiker aanmaken", "/views/AddUser.fxml", event);
     }
 
     @FXML
-    void onCreateLectureClick(ActionEvent event) throws IOException {
-        FXUtils.loadComponent("Les aanmaken","/views/CreateLecture.fxml", event);
-    }
-
-    @FXML
-    void onCreateUserClick(ActionEvent event) throws IOException {
-        FXUtils.loadComponent("Gebruiker aanmaken","/views/AddUser.fxml", event);
-    }
-
     public void onLogOutClick(ActionEvent event) throws IOException {
-        FXUtils.loadComponent("Rol selectie","/views/RoleSelection.fxml", event);
+        FXUtils.loadComponent("Rol selectie", "/views/RoleSelection.fxml", event);
     }
 }
