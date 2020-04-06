fe0f329b2470d1e9a09820acc4281abe615aafaf
Xander Vedder <xander.vedd@gmail.com>
2020-04-06 14:01:17 +0200

    Uncomment Controllers

--- src/controllers/TeacherShowLecturesController.java
+++ src/controllers/TeacherShowLecturesController.java
@@ -1,30 +1,30 @@
-//package controllers;
-//
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.scene.control.DatePicker;
-//import javafx.scene.control.ListView;
-//import javafx.scene.control.skin.DatePickerSkin;
-//import javafx.scene.layout.AnchorPane;
-//import javafx.scene.layout.StackPane;
-//
-//public class TeacherShowLecturesController {
-//
-//    @FXML private AnchorPane datePickerAnchorPane;
-//    @FXML private DatePicker datePicker;
-//    @FXML private ListView showLecturesListView;
-//
-//    @FXML
-//    public void initialize() {
-//        datePicker.setVisible(false);
-//        datePicker.setManaged(false);
-//        DatePickerSkin skin = new DatePickerSkin(datePicker);
-//        datePickerAnchorPane.getChildren().add(skin.getPopupContent());
-//    }
-//
-//    public void onCancelPressed(ActionEvent actionEvent) {
-//    }
-//
-//    public void onShowInfoButtonPressed(ActionEvent actionEvent) {
-//    }
-//}
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.skin.DatePickerSkin;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+public class TeacherShowLecturesController {
+
+    @FXML private AnchorPane datePickerAnchorPane;
+    @FXML private DatePicker datePicker;
+    @FXML private ListView showLecturesListView;
+
+    @FXML
+    public void initialize() {
+        datePicker.setVisible(false);
+        datePicker.setManaged(false);
+        DatePickerSkin skin = new DatePickerSkin(datePicker);
+        datePickerAnchorPane.getChildren().add(skin.getPopupContent());
+    }
+
+    public void onCancelPressed(ActionEvent actionEvent) {
+    }
+
+    public void onShowInfoButtonPressed(ActionEvent actionEvent) {
+    }
+}
