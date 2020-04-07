fc6f06dce7a93187757de24995d24fe194e4330e
JortWillemsen <jort@maurice-nienke.nl>
2020-04-08 01:07:30 +0200

    Implement full darkmode

--- src/controllers/MytendanceController.java
+++ src/controllers/MytendanceController.java
@@ -68,8 +68,6 @@ public class MytendanceController {
         FXUtils.loadButtonComponent("Statistiek",
                 event -> FXUtils.loadPaneIntoView(this.FXMLContainer, "/views/AttendanceOverview.fxml"), 0, this.children, this.group);
         FXUtils.loadButtonComponent("Overzicht Lessen",
-                event -> FXUtils.loadPaneIntoView(this.FXMLContainer, "/views/AbsenceOverview.fxml"), 0, this.children, this.group);
-        FXUtils.loadButtonComponent("Overzicht Lessen",
                 event -> FXUtils.loadPaneIntoView(this.FXMLContainer, "/views/TeacherShowLectures.fxml"), 0, this.children, this.group);
         FXUtils.loadButtonComponent("Les aanmaken",
                 event -> FXUtils.loadPaneIntoView(this.FXMLContainer, "/views/CreateLecture.fxml"), 0, this.children, this.group);
